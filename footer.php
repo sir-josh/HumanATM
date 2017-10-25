fb9ebbeba079fea3ccaccb3d54d52f14c64875ca
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2017-10-25 16:32:48 +0100

    HumanATM-header and footer
    This contains the header.php, footer.php and css for both of them i.e header-footer.css
    Call the footer.php inside the header.php file to view it using the php "include" tag

---
+++ footer.php
@@ -0,0 +1,40 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <nav class="navbar">
+                    <div class="col-md-4 col-sm-12 navbar-header">
+                        <a id="header-color"">
+                            <img style="width: 45px;" src="logo.png" alt="">
+                            Human ATM</a>
+                    </div>
+
+                    <div id="footer-nav" class="col-md-8 col-sm-12">
+
+                            <div class="col-md-3 col-sm-12"><a href="#">Wallet</a></div>
+                            <div class="col-md-3 col-sm-12"><a href="#">Transactions</a></div>
+                            <div class="col-md-3 col-sm-12"><a href="#">Pay Bills</a></div>
+                            <div  id="invite-friends-button" class="col-md-3 col-sm-12 btn btn-default"><a href="#">Invite Friends</a></div>
+                    </div>
+
+            </nav><hr>
+            <div class="socials">
+                <span><img class="img-circle" src="twitter-logo.png"></span>
+                <span><img class="img-circle" src="facebook-logo.png"></span>
+                <span><img class="img-circle" src="instagram-logo.png"></span>
+            </div>
+
+            <div class="foot-note">
+                <ul class="list-inline">
+                    <li>&copy;2017 HumanATM</li>
+                    <li><a>Get Help</a></li>
+                    <li> <a>Privacy Statement</a></li>
+                    <li> <a>Terms of Use</a></li>
+                    <li> <a>Code of Conduct</a></li>
+                    <li> Version 1.0.2017</li>
+                </ul>
+            </div>
+
+    </div>
+    </div>
+
+</div>
---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>humanATM</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="header-footer.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+
+
+
+<!-- Navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #0F3B75;">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#HumanATMNavbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a id="header-color" class="navbar-brand" href="#">
+                <img style="width:45px;height:45px;padding:5px;" src="logo.png" alt="">
+                Human ATM</a>
+        </div>
+        <div class="collapse navbar-collapse" id="HumanATMNavbar">
+            <ul class="nav navbar-nav navbar-right" id="header-color" >
+                <li><a href="#">WALLET</a></li>
+                <li><a href="#">TRANSACTIONS</a></li>
+                <li><a href="#">PAY BILLS</a></li>
+                <li><a href="#" >INVITE FRIENDS</a></li>
+            </ul>
+        </div>
+    </div>
+</nav><!-- Navbar Ends -->
+
+<!-- Put the body content here -->
+
+
+<!-- Footer -->
+<?php include"footer.php"; ?>
+<!-- End footer>
